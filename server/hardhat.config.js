20c3be30e0d603ce9c8909596787bc90e76f9d4d
Arnav Kirti <arnav.tkd@gmail.com>
2024-01-14 07:18:18 +0530

    commit

---
+++ server/hardhat.config.js
@@ -0,0 +1,23 @@
+const { task } = require("hardhat/config");
+
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for(const account of accounts){
+    console.log(account.address);
+  }
+})
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_URL,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+    } 
+  }
+};
